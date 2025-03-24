c7b62bd1a2d16dd07eb2e6ae077b7d817314146d
LukaSkop <lukaskopljakovic@gmail.com>
2025-03-24 13:11:59 +0100

    Implement Scoreboard class with startMatch

---
+++ src/main/java/com/scoreboard/Scoreboard.java
@@ -0,0 +1,54 @@
+package com.scoreboard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scoreboard {
+    private final List<Match> matches;
+
+    public Scoreboard(){
+        matches = new ArrayList<>();
+    }
+
+    public void startMatch(String homeTeam, String awayTeam){
+        for (Match match : matches){
+             if(match.getHomeTeam().equals(homeTeam) && match.getAwayTeam().equals(awayTeam)){
+                 System.out.println("Match already exists.");
+                 return;
+             }
+            }
+        matches.add(new Match(homeTeam, awayTeam));
+        }
+
+    public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore){
+        for (Match match : matches) {
+            if(match.getHomeTeam().equals(homeTeam) && match.getAwayTeam().equals(awayTeam)){
+                match.updateScore(homeScore, awayScore);
+                return;
+            }
+        }
+        System.out.println("No ongoing match found for " + homeTeam + " vs " + awayTeam +"!");
+    }
+
+    public void finishMatch(String homeTeam, String awayTeam){
+        matches.removeIf(match -> match.getHomeTeam().equals(homeTeam) && match.getAwayTeam().equals(awayTeam));
+    }
+
+    public List<String> getSummary() {
+        List<Match> sortedMatches = new ArrayList<>(matches);
+        sortedMatches.sort((m1, m2) -> {
+            int totalScoreComparison = Integer.compare(m2.getTotalScore(), m1.getTotalScore());
+            if (totalScoreComparison != 0) {
+                return totalScoreComparison;
+            }
+            return m2.getStartTime().compareTo(m1.getStartTime());
+        });
+
+        List<String> summary = new ArrayList<>();
+        for (Match match : sortedMatches) {
+            summary.add(match.getMatchDetails());
+        }
+        return summary;
+    }
+}
+

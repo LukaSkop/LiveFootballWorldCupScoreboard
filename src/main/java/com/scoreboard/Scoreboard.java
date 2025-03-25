de765940dbb66abec8bd14da7e5e296737635197
LukaSkop <lukaskopljakovic@gmail.com>
2025-03-25 13:45:42 +0100

    Added new edge cases to handle: - Invalid team names - Invalid start time - Team cannot play against its self - Scores cannot be negative - Cannot finish a match that doesn't exist

--- src/main/java/com/scoreboard/Scoreboard.java
+++ src/main/java/com/scoreboard/Scoreboard.java
@@ -11,30 +11,48 @@ public class Scoreboard {
         matches = new ArrayList<>();
     }
 
+    //Method to start match
     public void startMatch(String homeTeam, String awayTeam, LocalDateTime startTime){
         for (Match match : matches){
              if(match.getHomeTeam().equals(homeTeam) && match.getAwayTeam().equals(awayTeam)){
-                 System.out.println("Match already exists.");
-                 return;
+                 throw new IllegalArgumentException("Match already exists.");
+             }
+             if(homeTeam == null || awayTeam == null || homeTeam.trim().isEmpty() || awayTeam.trim().isEmpty()){
+                 throw new IllegalArgumentException("Invalid team names: Names cannot be empty.");
+             }
+             if (startTime == null) {
+                throw new IllegalArgumentException("Invalid start time: Cannot be null.");
+             }
+             if(homeTeam.equals(awayTeam)){
+                 throw new IllegalArgumentException("A team cannot play against itself.");
              }
             }
         matches.add(new Match(homeTeam, awayTeam, startTime));
         }
 
+    //Method to update score
     public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore){
+        if (homeScore < 0 || awayScore < 0) {
+            throw new IllegalArgumentException("Scores cannot be negative.");
+        }
         for (Match match : matches) {
             if(match.getHomeTeam().equals(homeTeam) && match.getAwayTeam().equals(awayTeam)){
                 match.updateScore(homeScore, awayScore);
                 return;
             }
         }
-        System.out.println("No ongoing match found for " + homeTeam + " vs " + awayTeam +"!");
+        throw new IllegalArgumentException("No ongoing match found for " + homeTeam + " vs " + awayTeam +"!");
     }
 
+    //Method to finish match
     public void finishMatch(String homeTeam, String awayTeam){
-        matches.removeIf(match -> match.getHomeTeam().equals(homeTeam) && match.getAwayTeam().equals(awayTeam));
+        boolean removed = matches.removeIf(match -> match.getHomeTeam().equals(homeTeam) && match.getAwayTeam().equals(awayTeam));
+        if (!removed){
+            throw new IllegalArgumentException("No such match is ongoing.");
+        }
     }
 
+    //Get summary method
     public List<String> getSummary() {
         List<Match> sortedMatches = new ArrayList<>(matches);
         sortedMatches.sort((m1, m2) -> {
